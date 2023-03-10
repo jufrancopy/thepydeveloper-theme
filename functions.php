3fa4a5cd1508b2d1608d42ec9c68b456fbdd6770
jufrancopy <jucfra23@gmail.com>
2023-03-10 13:56:26 -0300

    Actualización de Plantilla - Inlcusion de Mejoras

--- functions.php
+++ functions.php
@@ -178,56 +178,56 @@ function post_about_us()
 }
 add_action('init', 'post_about_us', 0);
 
-// function video_function()
-// {
-// 	add_theme_support('post-thumbnails');
+function video_function()
+{
+	add_theme_support('post-thumbnails');
 
-// 	// Asignamos entre comillados el nombre que llevara la funcion especial.
-// 	register_post_type('videos', array(
-// 		'labels' => array(
-// 			'name' => 'Video Promocional',
-// 			// 'add_new_item' => 'Agregar Testimonio'
-// 		),
-// 		'public' => true,
-// 		'menu_icon' => 'dashicons-heart',
-// 		'show_ui' => true,
-// 		'show_in_menu' => 'themes.php',
-// 		'supports' => array('title', 'editor', 'custom-fields')
-// 	));
-// }
-// add_action('after_setup_theme', video_function());
+	// Asignamos entre comillados el nombre que llevara la funcion especial.
+	register_post_type('videos', array(
+		'labels' => array(
+			'name' => 'Video Promocional',
+			// 'add_new_item' => 'Agregar Testimonio'
+		),
+		'public' => true,
+		'menu_icon' => 'dashicons-heart',
+		'show_ui' => true,
+		'show_in_menu' => 'themes.php',
+		'supports' => array('title', 'editor', 'custom-fields')
+	));
+}
+add_action('after_setup_theme', video_function());
 
-// function network_update_function()
-// {
-// 	add_theme_support('post-thumbnails');
+function network_update_function()
+{
+	add_theme_support('post-thumbnails');
 
-// 	register_post_type('networkUpdate', array(
-// 		'labels' => array(
-// 			'name' => 'Visitas',
-// 			'add_new_item' => 'Agregar Nueva Entrada'
-// 		),
-// 		'public' => true,
-// 		'menu_icon' => 'dashicons-networking',
-// 		'show_ui' => true,
-// 		'show_in_menu' => 'themes.php',
-// 		'supports' => array('title', 'editor', 'thumbnail')
-// 	));
-// }
-// add_action('after_setup_theme', network_update_function());
+	register_post_type('networkUpdate', array(
+		'labels' => array(
+			'name' => 'Visitas',
+			'add_new_item' => 'Agregar Nueva Entrada'
+		),
+		'public' => true,
+		'menu_icon' => 'dashicons-networking',
+		'show_ui' => true,
+		'show_in_menu' => 'themes.php',
+		'supports' => array('title', 'editor', 'thumbnail')
+	));
+}
+add_action('after_setup_theme', network_update_function());
 
-// function testimonials_function()
-// {
-// 	add_theme_support('post-thumbnails');
-// 	register_post_type('testimonials', array(
-// 		'labels' => array(
-// 			'name' => 'Testimonios',
-// 			'add_new_item' => 'Agregar Testimonio'
-// 		),
-// 		'public' => true,
-// 		'menu_icon' => 'dashicons-heart',
-// 		'show_ui' => true,
-// 		'show_in_menu' => 'themes.php',
-// 		'supports' => array('title', 'editor', 'thumbnail')
-// 	));
-// }
-// add_action('after_setup_theme', testimonials_function());
+function testimonials_function()
+{
+	add_theme_support('post-thumbnails');
+	register_post_type('testimonials', array(
+		'labels' => array(
+			'name' => 'Testimonios',
+			'add_new_item' => 'Agregar Testimonio'
+		),
+		'public' => true,
+		'menu_icon' => 'dashicons-heart',
+		'show_ui' => true,
+		'show_in_menu' => 'themes.php',
+		'supports' => array('title', 'editor', 'thumbnail')
+	));
+}
+add_action('after_setup_theme', testimonials_function());
